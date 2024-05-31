7907ec413012258d264313a0dcf9c5e71d7308ed
Sennur Tas <77441376+ccsenny@users.noreply.github.com>
2024-05-31 13:54:00 +0200

    Adjust eref information for the ING bank. (#57)
    This bank gives the payment information in both the /EREF/ field and the
    /REMI/USTD// field.

--- tests/Jejik/Tests/MT940/Parser/IngTest.php
+++ tests/Jejik/Tests/MT940/Parser/IngTest.php
@@ -14,6 +14,9 @@ declare(strict_types=1);
 
 namespace Jejik\Tests\MT940\Parser;
 
+use Jejik\MT940\Balance;
+use Jejik\MT940\Exception\NoParserFoundException;
+use Jejik\MT940\Parser\Ing;
 use Jejik\MT940\Reader;
 use PHPUnit\Framework\TestCase;
 
@@ -24,6 +27,7 @@ use PHPUnit\Framework\TestCase;
  */
 class IngTest extends TestCase
 {
+
     /**
      * @dataProvider statementsProvider
      *
@@ -46,9 +50,9 @@ class IngTest extends TestCase
      */
     public function testBalance($statements)
     {
-        /** @var \Jejik\MT940\Balance $balance */
+        /** @var Balance $balance */
         $balance = $statements[0]->getOpeningBalance();
-        $this->assertInstanceOf(\Jejik\MT940\Balance::class, $balance);
+        $this->assertInstanceOf(Balance::class, $balance);
         $this->assertEquals('2010-07-22 00:00:00', $balance->getDate()->format('Y-m-d H:i:s'));
         $this->assertEquals('EUR', $balance->getCurrency());
         $this->assertEquals(0.0, $balance->getAmount());
@@ -58,23 +62,48 @@ class IngTest extends TestCase
      * @dataProvider statementsProvider
      *
      * @param array $statements
+     * @param int|null $numberOfTransactions
+     * @param string|null $bookDate
+     * @param string|null $valueDate
+     * @param float|null $amount
+     * @param array|null $transactionsData
      */
-    public function testTransaction($statements)
+    public function testTransaction(
+        array   $statements,
+        ?int    $numberOfTransactions,
+        ?string $bookDate,
+        ?string $valueDate,
+        ?float  $amount,
+        ?array  $transactionsData
+    ): void
     {
         $transactions = $statements[0]->getTransactions();
-        $this->assertCount(7, $transactions);
+        $this->assertCount($numberOfTransactions, $transactions);
 
-        $this->assertEquals('2010-07-22 00:00:00', $transactions[0]->getBookDate()->format('Y-m-d H:i:s'));
-        $this->assertEquals(null, $transactions[0]->getValueDate());
-        $this->assertEquals(-25.03, $transactions[0]->getAmount());
+        $this->assertEquals($bookDate, $transactions[0]->getBookDate()->format('Y-m-d H:i:s'));
+        $this->assertEquals($valueDate, $transactions[0]->getValueDate());
+        $this->assertEquals($amount, $transactions[0]->getAmount());
 
         $expected = "RC AFREKENING BETALINGSVERKEER\r\n"
-                  . "BETREFT REKENING 4715589 PERIODE: 01-10-2010 / 31-12-2010\r\n"
-                  . "ING Bank N.V. tarifering ING";
+            . "BETREFT REKENING 4715589 PERIODE: 01-10-2010 / 31-12-2010\r\n"
+            . "ING Bank N.V. tarifering ING";
 
-        $this->assertEquals($expected, $transactions[0]->getDescription());
+        if (!$transactionsData) {
+            $this->assertEquals($expected, $transactions[0]->getDescription());
+        }
+
         if (null !== $transactions[1]->getContraAccount()) {
             $this->assertEquals('0111111111', $transactions[1]->getContraAccount()->getNumber());
+        }
+
+        if ($transactionsData) {
+            for ($i = 0; $i < count($transactions); $i++) {
+                $transactionInformations = json_decode($transactionsData[$i], true);
+                foreach ($transactionInformations as $key => $value) {
+                    $method = 'get' . $key;
+                    $this->assertEquals($value, $transactions[$i]->$method());
+                }
+            }
         }
     }
 
@@ -86,26 +115,100 @@ class IngTest extends TestCase
     public function testBookDate($statements)
     {
         $transactions = $statements[0]->getTransactions();
-        if (null !== $transactions[6]->getValueDate()) {
-            $this->assertEquals('2010-07-22 00:00:00', $transactions[6]->getValueDate()->format('Y-m-d H:i:s'));
+        if (null !== $transactions[count($transactions)-1]->getValueDate()) {
+            $this->assertEquals('2010-07-22 00:00:00', $transactions[count($transactions)-1]->getValueDate()->format('Y-m-d H:i:s'));
         }
-        $this->assertEquals('2010-07-23 00:00:00', $transactions[6]->getBookDate()->format('Y-m-d H:i:s'));
+        $this->assertEquals('2010-07-23 00:00:00', $transactions[count($transactions)-1]->getBookDate()->format('Y-m-d H:i:s'));
     }
 
     /**
-     * @dataProvider statementsProvider
-     * @throws \Jejik\MT940\Exception\NoParserFoundException
+     * @throws NoParserFoundException
      */
     public function statementsProvider(): array
     {
         $reader = new Reader();
         $reader->addParser('Ing', \Jejik\MT940\Parser\Ing::class);
-        
-        return array(
-            array($reader->getStatements(file_get_contents(__DIR__ . '/../Fixture/document/ing-dos.txt'))),
-            array($reader->getStatements(file_get_contents(__DIR__ . '/../Fixture/document/ing-unix.txt'))),
-            array($reader->getStatements(file_get_contents(__DIR__ . '/../Fixture/document/ing-unix-2.txt'))),
-            array($reader->getStatements(file_get_contents(__DIR__ . '/../Fixture/document/ing-unix-3.txt'))),
-        );
+        return [
+            [
+                $reader->getStatements(file_get_contents(__DIR__ . '/../Fixture/document/ing-dos.txt')),
+                'number of transactions' => 7,
+                'bookDate' => '2010-07-22 00:00:00',
+                'valueDate' => null,
+                'amount' => -25.03,
+                []
+            ],
+            [
+                $reader->getStatements(file_get_contents(__DIR__ . '/../Fixture/document/ing-unix.txt')),
+                'number of transactions' => 7,
+                'bookDate' => '2010-07-22 00:00:00',
+                'valueDate' => null,
+                'amount' => -25.03,
+                []
+            ],
+            [
+                $reader->getStatements(file_get_contents(__DIR__ . '/../Fixture/document/ing-unix-2.txt')),
+                'number of transactions' => 7,
+                'bookDate' => '2010-07-22 00:00:00',
+                'valueDate' => null,
+                'amount' => -25.03,
+                []
+            ],
+            [
+                $reader->getStatements(file_get_contents(__DIR__ . '/../Fixture/document/ing-unix-3.txt')),
+                'number of transactions' => 7,
+                'bookDate' => '2010-07-22 00:00:00',
+                'valueDate' => null,
+                'amount' => -25.03,
+                []
+            ],
+            [
+                $reader->getStatements(file_get_contents(__DIR__ . '/../Fixture/document/ing-4.txt')),
+                'number of transactions' => 4,
+                'bookDate' => '2010-04-20 00:00:00',
+                'valueDate' => null,
+                'amount' => -768.51,
+                [
+                    '{
+                        "Code":"TRF", 
+                        "TransactionCode":"00200", 
+                        "TxText":"TOTAAL 5 VZ/", 
+                        "Eref":"TOTAAL 5 VZ/", 
+                        "Bic":null, 
+                        "Iban":null, 
+                        "AccountHolder":null, 
+                        "RawSubfieldsData":"/PREF/SEPA-20230418220132-60947700-P1//REMI/USTD//TOTAAL 5 VZ/"
+                    }',
+                    '{
+                        "Code":"TRF", 
+                        "TransactionCode":"00100", 
+                        "TxText":"Nr. LNT22000057 / 21.2.2023Nr. LNT22000079 / 21.3.2023Nr. LNT22000068 / 21.2.2023/", 
+                        "Eref":"Nr. LNT22000057 / 21.2.2023Nr. LNT22000079 / 21.3.2023Nr. LNT22000068 / 21.2.2023/", 
+                        "Bic":"ABNANL2A", 
+                        "Iban":"NL71ABNA0841238***", 
+                        "AccountHolder":"GREENFLUX ASSETS BV", 
+                        "RawSubfieldsData":"/CNTP/NL71ABNA0841238***/ABNANL2A/GREENFLUX ASSETS BV///REMI/USTD//Nr. LNT22000057 / 21.2.2023Nr. LNT22000079 / 21.3.2023Nr. LNT22000068 / 21.2.2023/"
+                    }',
+                    '{
+                        "Code":"TRF", 
+                        "TransactionCode":"00100", 
+                        "TxText":"110130590/", 
+                        "Eref":"SCT-1-BO230019-R1/ 110130590/", 
+                        "Bic":"RABONL2U", 
+                        "Iban":"NL68RABO0106200***", 
+                        "AccountHolder":"Lambrix Elektrotechniek", 
+                        "RawSubfieldsData":"/EREF/SCT-1-BO230019-R1//CNTP/NL68RABO0106200***/RABONL2U/Lambrix Elektrotechniek///REMI/USTD//110130590/"
+                    }',
+                    '{
+                        "Code":"CMI", 
+                        "TransactionCode":"05001", 
+                        "TxText":"POOL-M NL21INGB0650141172 POOL-S NL30INGB0008693687 PPM9455956 22/07/2010/", 
+                        "Eref":"POOL-M NL21INGB0650141172 POOL-S NL30INGB0008693687 PPM9455956 22/07/2010/", 
+                        "Bic":null, 
+                        "Iban":"NL21INGB0650141***", 
+                        "AccountHolder":"Rexel Holding Netherlands B.V.",
+                        "RawSubfieldsData":"/CNTP/NL21INGB0650141***//Rexel Holding Netherlands B.V.///REMI/USTD//POOL-M NL21INGB0650141172 POOL-S NL30INGB0008693687 PPM9455956 22/07/2010/"}',
+                ]
+            ],
+        ];
     }
 }
